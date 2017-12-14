829045de93efa5469971d56f7007b64ac4c17b28
Kudakwashe Chambwe <kaydee.kuda@gmail.com>
2017-12-15 00:58:49 +0100

    add sum function and jest and jslint file

---
+++ sum.test.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+const sum = require('./sum');
+
+test("should return sum correct", () => {
+    expect(sum(3,2)).toBe(5);
+    expect(sum(1,2)).toBe(3);
+});
+
+test("should return sum of 3 and 4", () => {
+    expect(sum(3,4)).toBe(7);
+});
+
+
+
